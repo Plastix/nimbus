51318aeac6e7806327bf1ebcd5daa3a7f6131f6b
Plastix <aidanpieper@gmail.com>
2015-07-20 17:32:20 -0400

    Clean up mcname and player stats plugins

--- plugins/mcname.py
+++ plugins/mcname.py
@@ -5,45 +5,15 @@ import requests
 import json
 from utils import valid_minecraft_username
 from plugin import CommandPlugin
+import logging
 
-mojang_profile_link = 'https://api.mojang.com/profiles/minecraft'
-
-
-def lookup_username(name):
-    if not valid_minecraft_username(name):
-        return build_slack_attachment(name, None, valid_name=False)
-
-    payload = json.dumps(name)
-    header = {'Content-type': 'application/json'}
-    r = requests.post(mojang_profile_link, headers=header, data=payload)
-
-    if r.status_code != requests.codes.ok:
-        print "Can't get lookup Minecraft username %s!" % name
-        return
-    else:
-        return build_slack_attachment(name, r.json())
-
-
-def build_slack_attachment(name, response, valid_name=True):
-    message = {
-        'title': 'Minecraft Username Lookup',
-        'mrkdwn_in': ['text']
-    }
-
-    if not valid_name:
-        message['text'] = ':warning: Username `%s` is not a valid username!' % name
-    elif not response:
-        message['text'] = ':white_check_mark: Username `%s` is available!' % name
-    elif 'legacy' in response[0]:
-        message['text'] = ':x: Username `%s` is taken but not premium!' % name
-    else:
-        message['text'] = ':x: Username `%s` is taken!' % name
-
-    return message
+log = logging.getLogger(__name__)
 
 
 class MCName(CommandPlugin):
     """Checks if a Minecraft username is registered"""
+
+    mojang_profile_link = 'https://api.mojang.com/profiles/minecraft'
 
     def __init__(self):
         CommandPlugin.__init__(self)
@@ -53,6 +23,42 @@ class MCName(CommandPlugin):
         self.help_example = ['!mcname Plastix', '!mcname Apple']
 
     def on_command(self, bot, event, response):
-        split = event['text'].split(' ')
-        response.update(attachments=json.dumps([lookup_username(split[0])]))
-        bot.sc.api_call('chat.postMessage', **response)
+        args = event['text']
+        if args:
+            parts = event['text'].split(' ')
+            response.update(attachments=json.dumps([MCName.lookup_username(parts[0])]))
+            bot.sc.api_call('chat.postMessage', **response)
+
+    @staticmethod
+    def lookup_username(name):
+        if not valid_minecraft_username(name):
+            return MCName.build_slack_attachment(name, None, valid_name=False)
+
+        payload = json.dumps(name)
+        header = {'Content-type': 'application/json'}
+        r = requests.post(MCName.mojang_profile_link, headers=header, data=payload)
+
+        if r.status_code != requests.codes.ok:
+            log.warning("Can't get lookup Minecraft username %s!" % name)
+            return
+        else:
+            return MCName.build_slack_attachment(name, r.json())
+
+    @staticmethod
+    def build_slack_attachment(name, response, valid_name=True):
+
+        message = {
+            'title': 'Minecraft Username Lookup',
+            'mrkdwn_in': ['text']
+        }
+
+        if not valid_name:
+            message['text'] = ':warning: Username `%s` is not a valid username!' % name
+        elif not response:
+            message['text'] = ':white_check_mark: Username `%s` is available!' % name
+        elif 'legacy' in response[0]:
+            message['text'] = ':x: Username `%s` is taken but not premium!' % name
+        else:
+            message['text'] = ':x: Username `%s` is taken!' % name
+
+        return message
--- plugins/player_stats.py
+++ plugins/player_stats.py
@@ -8,100 +8,6 @@ from utils import valid_minecraft_username
 from plugin import CommandPlugin
 
 
-def get_player_link(player_name):
-    return "http://oc.tc/" + str(player_name)
-
-
-def get_avatar_link(player_name):
-    return 'https://avatar.oc.tc/' + str(player_name) + '/16@2x.png'
-
-
-def scrape_stats(player_name):
-    """
-    Scrapes Overcast Network Player Stats
-    Based on code by @McSpider
-    https://github.com/McSpider/Overcast-IRC-Bot/blob/master/_functions/player.py
-    """
-    result = {}
-    result['name'] = player_name
-    result['error'] = ''
-
-    if not valid_minecraft_username(player_name):
-        result['error'] = ':warning: Invalid player name!'
-        return build_slack_attachment(result)
-
-    r = requests.get(get_player_link(player_name))
-
-    if r.status_code != requests.codes.ok:
-        if r.status_code == 404:
-            result['error'] = '404 - User not found'
-        if r.status_code == 522:
-            result['error'] = '522 - Request Timed Out'
-        else:
-            result['error'] = 'Request Exception - Code: ' + str(r.status_code)
-    else:
-        soup = BeautifulSoup(r.text)
-        if soup.find("h4", text=["Account Suspended"]):
-            result['error'] = 'User Account Suspended'
-        elif soup.find("p", text=["Page Exploded"]):
-            result['error'] = '404 - User not found'
-        elif soup.find("small", text=["server joins"]):
-            result['kills'] = soup.find("small", text=["kills"]).findParent('h2').contents[0].strip('\n')
-            result['deaths'] = soup.find("small", text=["deaths"]).findParent('h2').contents[0].strip('\n')
-            result['friends'] = soup.find("small", text=["friends"]).findParent('h2').contents[0].strip('\n')
-            result['kd_ratio'] = soup.find("small", text=["kd ratio"]).findParent('h2').contents[0].strip('\n')
-            result['kk_ratio'] = soup.find("small", text=["kk ratio"]).findParent('h2').contents[0].strip('\n')
-            result['joins'] = soup.find("small", text=["server joins"]).findParent('h2').contents[0].strip('\n')
-            result['raindrops'] = soup.find("small", text=["raindrops"]).findParent('h2').contents[0].strip('\n')
-
-            result['wools'] = "0"
-            result['cores'] = "0"
-            result['monuments'] = "0"
-
-            wools_element = soup.find("small", text=["wools placed"])
-            if wools_element:
-                result['wools'] = wools_element.findParent('h2').contents[0].strip('\n')
-
-            cores_element = soup.find("small", text=["cores leaked"])
-            if cores_element:
-                result['cores'] = cores_element.findParent('h2').contents[0].strip('\n')
-
-            monuments_element = soup.find("small", text=["monuments destroyed"])
-            if monuments_element:
-                result['monuments'] = monuments_element.findParent('h2').contents[0].strip('\n')
-        else:
-            result['error'] = 'Invalid user!'
-
-    return build_slack_attachment(result)
-
-
-def build_slack_attachment(stats):
-    """
-    Builds the JSON attachment for the Slack message
-    """
-    message = {}
-    error = stats['error']
-    name = stats['name']
-    message['author_name'] = name + ' (Player Stats)'
-
-    if error:
-        message['color'] = 'danger'
-        message['text'] = error
-    else:
-        message['color'] = 'good'
-        message['author_link'] = get_player_link(name)
-        message['author_icon'] = get_avatar_link(name)
-
-        text = 'Kills: `%s` Deaths: `%s` KD: `%s` KK: `%s`\nWools: `%s` Cores: `%s` Monuments: `%s`\nFriends: `%s` Joins: `%s` Raindrops: `%s`' % (
-            stats['kills'],
-            stats['deaths'], stats['kd_ratio'], stats['kk_ratio'], stats['wools'], stats['cores'], stats['monuments'],
-            stats['friends'], stats['joins'], stats['raindrops'])
-        message['text'] = text
-        message['mrkdwn_in'] = ['text']
-
-    return message
-
-
 class PlayerStats(CommandPlugin):
     """
     Scrapes Overcast Network player stats
@@ -117,6 +23,102 @@ class PlayerStats(CommandPlugin):
     def on_command(self, bot, event, response):
         args = event['text']
         if args:
-            message = scrape_stats(args)
+            message = PlayerStats.scrape_stats(args)
             response.update(attachments=json.dumps([message]))
             bot.sc.api_call('chat.postMessage', **response)
+
+    @staticmethod
+    def get_player_link(player_name):
+        return "http://oc.tc/" + str(player_name)
+
+    @staticmethod
+    def get_avatar_link(player_name):
+        return 'https://avatar.oc.tc/' + str(player_name) + '/16@2x.png'
+
+    @staticmethod
+    def scrape_stats(player_name):
+        """
+        Scrapes Overcast Network Player Stats
+        Based on code by @McSpider
+        https://github.com/McSpider/Overcast-IRC-Bot/blob/master/_functions/player.py
+        """
+        result = {
+            'name': player_name,
+            'error': ''
+        }
+
+        if not valid_minecraft_username(player_name):
+            result['error'] = ':warning: Invalid player name!'
+            return PlayerStats.build_slack_attachment(result)
+
+        r = requests.get(PlayerStats.get_player_link(player_name))
+
+        if r.status_code != requests.codes.ok:
+            if r.status_code == 404:
+                result['error'] = '404 - User not found'
+            if r.status_code == 522:
+                result['error'] = '522 - Request Timed Out'
+            else:
+                result['error'] = 'Request Exception - Code: ' + str(r.status_code)
+        else:
+            soup = BeautifulSoup(r.text)
+            if soup.find("h4", text=["Account Suspended"]):
+                result['error'] = 'User Account Suspended'
+            elif soup.find("p", text=["Page Exploded"]):
+                result['error'] = '404 - User not found'
+            elif soup.find("small", text=["server joins"]):
+                result['kills'] = soup.find("small", text=["kills"]).findParent('h2').contents[0].strip('\n')
+                result['deaths'] = soup.find("small", text=["deaths"]).findParent('h2').contents[0].strip('\n')
+                result['friends'] = soup.find("small", text=["friends"]).findParent('h2').contents[0].strip('\n')
+                result['kd_ratio'] = soup.find("small", text=["kd ratio"]).findParent('h2').contents[0].strip('\n')
+                result['kk_ratio'] = soup.find("small", text=["kk ratio"]).findParent('h2').contents[0].strip('\n')
+                result['joins'] = soup.find("small", text=["server joins"]).findParent('h2').contents[0].strip('\n')
+                result['raindrops'] = soup.find("small", text=["raindrops"]).findParent('h2').contents[0].strip('\n')
+
+                result['wools'] = "0"
+                result['cores'] = "0"
+                result['monuments'] = "0"
+
+                wools_element = soup.find("small", text=["wools placed"])
+                if wools_element:
+                    result['wools'] = wools_element.findParent('h2').contents[0].strip('\n')
+
+                cores_element = soup.find("small", text=["cores leaked"])
+                if cores_element:
+                    result['cores'] = cores_element.findParent('h2').contents[0].strip('\n')
+
+                monuments_element = soup.find("small", text=["monuments destroyed"])
+                if monuments_element:
+                    result['monuments'] = monuments_element.findParent('h2').contents[0].strip('\n')
+            else:
+                result['error'] = 'Invalid user!'
+
+        return PlayerStats.build_slack_attachment(result)
+
+    @staticmethod
+    def build_slack_attachment(stats):
+        """
+        Builds the JSON attachment for the Slack message
+        """
+        message = {}
+        error = stats['error']
+        name = stats['name']
+        message['author_name'] = name + ' (Player Stats)'
+
+        if error:
+            message['color'] = 'danger'
+            message['text'] = error
+        else:
+            message['color'] = 'good'
+            message['author_link'] = PlayerStats.get_player_link(name)
+            message['author_icon'] = PlayerStats.get_avatar_link(name)
+
+            text = 'Kills: `%s` Deaths: `%s` KD: `%s` KK: `%s`\nWools: `%s` Cores: `%s` Monuments: `%s`\nFriends: `%s` Joins: `%s` Raindrops: `%s`' % (
+                stats['kills'],
+                stats['deaths'], stats['kd_ratio'], stats['kk_ratio'], stats['wools'], stats['cores'],
+                stats['monuments'],
+                stats['friends'], stats['joins'], stats['raindrops'])
+            message['text'] = text
+            message['mrkdwn_in'] = ['text']
+
+        return message
